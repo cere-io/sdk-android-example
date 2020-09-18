259045e7a6437ff44d0cc4d3de194b8f605590bf
Rudolf Markulin <rudolf.markulin@protonmail.com>
2020-09-18 09:00:37 +0200

    Use OnInitializationErrorHandler

--- app/src/main/java/io/cere/sdk_android_example/CustomApplication.java
+++ app/src/main/java/io/cere/sdk_android_example/CustomApplication.java
@@ -1,11 +1,13 @@
 package io.cere.sdk_android_example;
 
 import android.app.Application;
+import android.util.Log;
 
 import io.cere.cere_sdk.CereModule;
 import io.cere.cere_sdk.InitStatus;
 
 public class CustomApplication extends Application {
+    private static String TAG = "CustomApplication";
     private CereModule cereModule = null;
     public void onCreate() {
         super.onCreate();
@@ -16,6 +18,9 @@ public class CustomApplication extends Application {
             this.cereModule.setOnInitializationFinishedHandler(() -> {
                 this.cereModule.sendEvent("APP_LAUNCHED_TEST", "{'locationId': 10}");
                 return;
+            });
+            this.cereModule.setOnInitializationErrorHandler((String error) -> {
+                    Log.e(TAG, error);
             });
             this.cereModule.init("242", "userID");
         }
