579d6a31e1289e1ea25eaef535563e4bd4cd2977
Oleg-Bogatyrov <oleg.bogatyrov@timebook.net>
2022-06-02 19:38:26 +0300

    Latest update for handling web view theme

--- app/src/main/java/io/cere/sdk_android_example/CustomApplication.java
+++ app/src/main/java/io/cere/sdk_android_example/CustomApplication.java
@@ -1,7 +1,12 @@
 package io.cere.sdk_android_example;
 
+import static android.view.ViewGroup.LayoutParams.FILL_PARENT;
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
+
 import android.app.Application;
+import android.content.res.Resources;
 import android.util.Log;
+import android.widget.RelativeLayout;
 
 import io.cere.cere_sdk.CereModule;
 import io.cere.cere_sdk.InitStatus;
@@ -15,14 +20,21 @@ public class CustomApplication extends Application {
             this.cereModule = CereModule.getInstance(this);
         } else {
             this.cereModule = CereModule.getInstance(this);
-//            this.cereModule.setOnInitializationFinishedHandler(() -> {
-//                this.cereModule.sendEventTrustedEvent("LIVE_ONE_CONTEXTUAL_ENTERED");
-//                return;
-//            });
             this.cereModule.setOnInitializationErrorHandler((String error) -> {
                     Log.e(TAG, error);
             });
-            //this.cereModule.init("242", "userID"); For version 1.0.0
+//            RelativeLayout.LayoutParams params = new  RelativeLayout.LayoutParams(RelativeLayout.CENTER_IN_PARENT, RelativeLayout.TRUE);
+//            params.width = MATCH_PARENT;
+//            params.height = MATCH_PARENT;
+
+
+            this.cereModule.setTheme(R.style.CereEventTheme);
+
+//            this.cereModule.webview.setInitialScale(80);
+
+//            this.cereModule.setLayout(params);
+//            this.cereModule.webview.layout(-10, -10, -10, 10);
+
             this.cereModule.init("2354", "", "1234567890", "112112", "TRUSTED_3RD_PARTY");
         }
     }
