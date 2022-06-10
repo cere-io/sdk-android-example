2bb93c1f36229aef972e119c598cc3915c613370
Oleg-Bogatyrov <oleg.bogatyrov@timebook.net>
2022-06-10 14:44:27 +0300

    JS event handling added

--- app/src/main/java/io/cere/sdk_android_example/CustomApplication.java
+++ app/src/main/java/io/cere/sdk_android_example/CustomApplication.java
@@ -3,13 +3,17 @@ package io.cere.sdk_android_example;
 import static android.view.ViewGroup.LayoutParams.FILL_PARENT;
 import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.res.Resources;
 import android.util.Log;
 import android.widget.RelativeLayout;
 
+import androidx.annotation.NonNull;
+
 import io.cere.cere_sdk.CereModule;
 import io.cere.cere_sdk.InitStatus;
+import io.cere.cere_sdk.JSEventListener;
 
 public class CustomApplication extends Application {
     private static String TAG = "CustomApplication";
@@ -35,7 +39,14 @@ public class CustomApplication extends Application {
 //            this.cereModule.setLayout(params);
 //            this.cereModule.webview.layout(-10, -10, -10, 10);
 
-            this.cereModule.init("2096", "", "1234567890", "112112112", "TRUSTED_3RD_PARTY", "stage");
+            this.cereModule.init("2354", "", "1234567890", "112112112", "TRUSTED_3RD_PARTY", "dev");
+            this.cereModule.attachEventListener((s, activity) -> {
+                switch(s){
+                    case "HEADER_CLICKED":
+                        activity.finish();
+                        break;
+                }
+            });
         }
     }
 }
